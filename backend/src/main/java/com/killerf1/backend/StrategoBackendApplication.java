caf6a446cf51e18a5a8519359f06d54d77f199f3
Weiyi <zhuwy99@gmail.com>
2023-08-21 16:09:27 -0700

    Created boilerplate for frontend and backend

---
+++ backend/src/main/java/com/killerf1/backend/StrategoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.killerf1.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategoBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/killerf1/backend/StrategoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killerf1.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

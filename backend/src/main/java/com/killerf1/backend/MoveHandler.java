53472a463baff046b8c7d08d6120d67a22cbd02d
Weiyi <zhuwy99@gmail.com>
2023-08-27 16:13:12 -0700

    chore: add logging

--- backend/src/main/java/com/killerf1/backend/MoveHandler.java
+++ backend/src/main/java/com/killerf1/backend/MoveHandler.java
@@ -2,6 +2,8 @@ package com.killerf1.backend;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -12,15 +14,18 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
  */
 public class MoveHandler extends TextWebSocketHandler {
 
+    private static final Logger logger = LogManager.getLogger();
+
     /**
      * Handles incoming client messages and updates the game state accordingly
      * 
-     * @param session The websocket session where clients and the server communicates over
+     * @param session The websocket session where clients and the server
+     *                communicates over
      * @param message The text message clients send
      */
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {
-        System.out.println("message received: " + message.toString());
+        logger.info("Message received: " + message.toString());
         try {
             session.sendMessage(new TextMessage("message received"));
         } catch (IOException e) {
@@ -30,16 +35,16 @@ public class MoveHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-        System.out.println("connection established");
+        logger.info("Connection established with " + session.getRemoteAddress());
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        System.out.println("connection closed");
+        logger.info("Connection closed with " + session.getRemoteAddress());
     }
 
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) {
-        System.out.println("transport error");
+        logger.error("Transport error: " + exception.toString());
     }
 }
